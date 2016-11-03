932ae34ea654a6a1281691304274c4fcf270c093
austin <austin@campuswise.com>
2016-11-02 23:40:03 -0400

    Oh god why

---
+++ draw.py
@@ -0,0 +1,99 @@
+import sys
+import math
+from turtle import Turtle, mainloop
+
+
+class Drawer:
+
+    def __init__(self):
+        super().__init__()
+        self.t = Turtle()
+        self.t.hideturtle()
+        self.screen = self.t.getscreen()
+        self.screen.setup(width=600, height=400, startx=300, starty=200)
+        self.t.pensize(1)
+        self.t.color("black")
+        self.screen.onkey(self.clear, "c")
+        self.screen.onkey(self.quit, "q")
+        self.a_x = 0
+        self.a_y = 0
+        self.b_x = 0
+        self.b_y = 0
+        self.radius = 0
+        self.t.speed(0)
+        self.screen.listen()
+        self.screen.onclick(self.set_a)
+
+    def clear(self):
+        self.screen.clear()
+        self.__init__()
+
+    def quit(self):
+        self.screen.bye()
+        sys.exit(0)
+
+    def set_a(self, x, y):
+        self.t.penup()
+        self.a_x = x
+        self.a_y = y
+        print(x, y)
+        self.t.goto(x, y)
+        self.t.dot()
+        self.screen.onclick(self.set_b)
+
+    # This function continues execution after set_a
+    def set_b(self, x, y):
+        self.t.fillcolor("red")
+        self.b_x = x
+        self.b_y = y
+        print(x, y)
+        self.t.goto(x, y)
+        self.t.dot()
+        self.radius = math.hypot(self.a_x - self.b_x, self.a_y - self.b_y)
+        self.t.goto(self.a_x, self.a_y - self.radius)
+        self.t.setheading(0)
+        self.t.pendown()
+        self.t.begin_fill()
+        self.t.circle(self.radius)
+        self.t.end_fill()
+        self.t.penup()
+        self.t.goto(self.a_x, self.a_y + 1.23 * self.radius)
+        self.t.pendown()
+        self.t.fillcolor("green")
+        self.t.begin_fill()
+        self.t.right(36)  # half a full angle
+        self.t.forward(1.452 * self.radius)
+        for i in range(4):
+            self.t.right(72)
+            self.t.forward(1.452 * self.radius)  # formula for side of pentagon
+
+        self.t.end_fill()
+        self.t.penup()
+        self.t.goto(self.a_x, self.a_y + self.radius)
+        self.t.fillcolor("black")
+        self.t.begin_fill()
+        for i in range(5):
+            self.t.right(72)
+            self.t.forward(1.18 * self.radius)
+
+        self.t.end_fill()
+        self.t.pendown()
+        self.__init__()
+
+    def main(self):
+        mainloop()
+
+
+def draw_polygon(turtle, side_length, num_sides):
+    turn_angle = 360 / num_sides
+    for i in range(num_sides):
+        turtle.left(turn_angle)
+        turtle.forward(side_length)
+# def select_color():
+
+
+def draw():
+    drawer = Drawer()
+    drawer.main()
+
+draw()
